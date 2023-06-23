8e86802c16036f0b1024026639f842f0b9f20577
laibasidd <132685735+laibasidd@users.noreply.github.com>
2023-06-23 14:02:00 +0500

    Add files via upload

---
+++ mern/index1 .js
@@ -0,0 +1,8 @@
+let myarr = ["ii1.jpg","ii2.jpg","gg.png"];
+function myimage(){
+    document.getElementById('images').src = myarr[0];
+}
+function mybutton(){
+    document.getElementById('btn2');
+    alert('save successfully');
+}
